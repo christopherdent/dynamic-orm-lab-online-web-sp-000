c6a56642ed06b703beaed6de9d8d23ab7c507902
Christopher Dent <christopherdent01@gmail.com>
2019-11-03 23:07:51 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -58,7 +58,7 @@ class InteractiveRecord
     column_name = attribute.keys[0].to_s
     value_name = attribute.values[0]
     
-    sql = "SELECT * FROM #{self.table_name} WHERE column_name = ?"  
+    sql = "SELECT * FROM #{self.table_name} WHERE #{column_name} = ?"  
 
       DB[:conn].execute(sql)
      
